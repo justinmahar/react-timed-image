832b283dc6f7ad587a3a8350d3cef8c8c8b0f671
Justin Mahar <contact@justinmahar.com>
2024-02-09 15:47:45 -0500

    Run updater

--- dist/stories/TimedImage.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { TimedImage } from '../components/TimedImage';
-declare const _default: ComponentMeta<typeof TimedImage>;
-export default _default;
-export declare const TimedImageStory: ComponentStory<typeof TimedImage>;
--- src/stories/TimedImage.stories.tsx
+++ src/stories/TimedImage.stories.tsx
@@ -1,41 +1,39 @@
-/*
- * More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
- * More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
- * More on args: https://storybook.js.org/docs/react/writing-stories/args
- * More on argTypes: https://storybook.js.org/docs/react/api/argtypes
- */
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-
 import { TimedImage } from '../components/TimedImage';
+import type { Meta, StoryObj } from '@storybook/react';
 
-export default {
-  title: 'Stories/TimedImage',
-  component: TimedImage,
-} as ComponentMeta<typeof TimedImage>;
-
-const Template: ComponentStory<typeof TimedImage> = (args) => {
-  const src = 'https://loremflickr.com/220/140';
-  return (
-    <>
-      <p>Reloads every 3 seconds -- uncached, will change:</p>
-      <TimedImage src={src} interval={3000} />
-      <pre>{`<TimedImage src="https://loremflickr.com/220/140" interval={3000} />`}</pre>
-      <p>
-        When uncached (default), a cache busting query param is attached to the src attribute. In most circumstances,
-        this causes the browser to retrieve the image again.
-      </p>
-      <hr />
-      <p>Reloads every 5 seconds (default time) -- cached, will not change:</p>
-      <TimedImage src={src} uncached={false} />
-      <pre>{`<TimedImage src="https://loremflickr.com/220/140" uncached={false} />`}</pre>
-      <p>Disabling the cache buster typically prevents the browser from retrieving the image again.</p>
-    </>
-  );
+// === Setup ===
+const StoryComponent = TimedImage; // <-- Set to your component
+const meta: Meta<typeof StoryComponent> = {
+  title: 'Stories/TimedImage', // <-- Set to your story title
+  component: StoryComponent,
+  parameters: {
+    options: { showPanel: false }, // Don't show addons panel
+  },
 };
+export default meta;
+type Story = StoryObj<typeof meta>;
 
-export const TimedImageStory = Template.bind({});
-TimedImageStory.args = {};
-TimedImageStory.story = {
+// === Stories ===
+export const Hello: Story = {
   name: 'Cached and Uncached',
+  render: () => {
+    const src = 'https://loremflickr.com/220/140';
+    return (
+      <>
+        <p>Reloads every 3 seconds -- uncached, will change:</p>
+        <TimedImage src={src} interval={3000} />
+        <pre>{`<TimedImage src="https://loremflickr.com/220/140" interval={3000} />`}</pre>
+        <p>
+          When uncached (default), a cache busting query param is attached to the src attribute. In most circumstances,
+          this causes the browser to retrieve the image again.
+        </p>
+        <hr />
+        <p>Reloads every 5 seconds (default time) -- cached, will not change:</p>
+        <TimedImage src={src} uncached={false} />
+        <pre>{`<TimedImage src="https://loremflickr.com/220/140" uncached={false} />`}</pre>
+        <p>Disabling the cache buster typically prevents the browser from retrieving the image again.</p>
+      </>
+    );
+  },
 };
